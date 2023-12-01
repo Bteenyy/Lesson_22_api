d8b7224c4bc4051096de394271c23f12341c5766
Rashit Sakhbutdinov <rasitsahbutdinov915455@gmail.com>
2023-12-01 17:09:29 +0300

    add allure

--- src/test/java/api/LoginWithApi.java
+++ src/test/java/api/LoginWithApi.java
@@ -15,29 +15,17 @@ import static spec.SuccessfulRegisterSpec.successfulResponseSpec;
 
 public class LoginWithApi extends TestBase {
     @Test
-    void successfulRegisterTest() {
-        SuccessfulRegisterBodyModel successfulRegisterBodyModel = new SuccessfulRegisterBodyModel();
-        successfulRegisterBodyModel.setUserName("Bteeny");
-        successfulRegisterBodyModel.setPassword("Test(111)^");
-        SuccessfulRegisterResponseModel response = given(successfulRequestSpec)
-                .body(successfulRegisterBodyModel)
+    void addBooks() {
+        AddBookBodyModel addBook = new AddBookBodyModel();
+        // addBook.setId();
+        addBook.getCollectionOfIsbns().setIsbn("9781449325862");
+        AddBookResponceModel responseBook = given(addBookRequestSpec)
+                .body(addBook)
                 .when()
-                .post("/Account/v1/Login")
+                .post("BookStore/v1/Books")
                 .then()
-                .spec(successfulResponseSpec)
-                .extract().as(SuccessfulRegisterResponseModel.class);
-}
-@Test
-        void addBooks() {
-    AddBookBodyModel addBook = new AddBookBodyModel();
-    addBook.setId();
-    addBook.getCollectionOfIsbns().setIsbn("9781449325862");
-    AddBookResponceModel responseBook = given(addBookRequestSpec)
-            .body(addBook)
-            .when()
-            .post("BookStore/v1/Books")
-            .then()
-            .spec(addBookResponseSpec)
-            .extract().as(AddBookResponceModel.class);
-}
+                .spec(addBookResponseSpec)
+                .extract().as(AddBookResponceModel.class);
+    }
+
 }
--- src/test/java/tests/FirstTest.java
+++ src/test/java/tests/FirstTest.java
@@ -12,22 +12,30 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static io.restassured.RestAssured.given;
-import static spec.AddBookSpec.addBookRequestSpec;
-import static spec.AddBookSpec.addBookResponseSpec;
 import static spec.SuccessfulRegisterSpec.successfulRequestSpec;
 import static spec.SuccessfulRegisterSpec.successfulResponseSpec;
 
 public class FirstTest extends TestBase {
     @Test
-            void checkTest(){
+    void successfulRegisterTest() {
+        SuccessfulRegisterBodyModel successfulRegisterBodyModel = new SuccessfulRegisterBodyModel();
+        successfulRegisterBodyModel.setUserName("Bteeny");
+        successfulRegisterBodyModel.setPassword("Test(111)^");
+        SuccessfulRegisterResponseModel response = given(successfulRequestSpec)
+                .body(successfulRegisterBodyModel)
+                .when()
+                .post("/Account/v1/Login")
+                .then()
+                .spec(successfulResponseSpec)
+                .extract().as(SuccessfulRegisterResponseModel.class);
         open("/favicon.ico");
-            //getWebDriver().manage().addCookie(new Cookie("userID", response.getUserId()));
-         //   getWebDriver().manage().addCookie(new Cookie("expires", response.getExpires()));
-           // getWebDriver().manage().addCookie(new Cookie("token", response.getToken()));
-           // getWebDriver().manage().addCookie(new Cookie("isbn",responseBook.getIsbn()));
-            open("/profile");
-            $("#userName-value").shouldHave(Condition.text("Bteeny"));
-        }
+        getWebDriver().manage().addCookie(new Cookie("userID", response.getUserId()));
+        getWebDriver().manage().addCookie(new Cookie("expires", response.getExpires()));
+        getWebDriver().manage().addCookie(new Cookie("token", response.getToken()));
+        // getWebDriver().manage().addCookie(new Cookie("isbn",responseBook.getIsbn()));
+        open("/profile");
+        $("#userName-value").shouldHave(Condition.text("Bteeny"));
+    }
 
 
 }
